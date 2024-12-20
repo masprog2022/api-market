96677d834387a0710a9c316377f5c3dcda188698
Mauro Manuel <mauro.manuel@unitel.co.ao>
2024-12-20 15:59:10 +0100

    feat: create ProductService

--- src/main/java/com/masprog/ice_market_api/services/ProductService.java
+++ src/main/java/com/masprog/ice_market_api/services/ProductService.java
@@ -1,4 +1,9 @@
 package com.masprog.ice_market_api.services;
 
+import com.masprog.ice_market_api.payload.ProductDTO;
+
 public interface ProductService {
+
+    ProductDTO addProduct(ProductDTO product);
+
 }
