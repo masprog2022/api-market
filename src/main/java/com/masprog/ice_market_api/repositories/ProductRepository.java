a698abf9bcd5b0cc82506aed3a8f87616c78776a
Mauro Manuel <mauro.manuel@unitel.co.ao>
2024-12-20 15:56:06 +0100

    feat: added method findByProductName

--- src/main/java/com/masprog/ice_market_api/repositories/ProductRepository.java
+++ src/main/java/com/masprog/ice_market_api/repositories/ProductRepository.java
@@ -1,9 +1,10 @@
-package com.masprog.ice_market_api.repository;
+package com.masprog.ice_market_api.repositories;
 
-import com.masprog.ice_market_api.model.Product;
+import com.masprog.ice_market_api.models.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
+    Product findByProductName(String productName);
 }
