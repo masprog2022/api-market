424bc45abf32b2f2c73aa491f94fcf9822dc0c7a
Mauro Manuel <mauro.manuel@unitel.co.ao>
2024-12-20 15:29:22 +0100

    feat: create product repository

--- src/main/java/com/masprog/ice_market_api/repository/ProductRepository.java
+++ src/main/java/com/masprog/ice_market_api/repository/ProductRepository.java
@@ -1,4 +1,9 @@
 package com.masprog.ice_market_api.repository;
 
-public interface ProductRepository {
+import com.masprog.ice_market_api.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
 }
