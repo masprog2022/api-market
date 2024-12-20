e5ed40755b2d122c053a6bd409dbc3ad5e296aa6
Mauro Manuel <mauro.manuel@unitel.co.ao>
2024-12-20 18:58:31 +0100

    fix: product remove methods lombok, and add tradiconal methods

--- src/main/java/com/masprog/ice_market_api/models/Product.java
+++ src/main/java/com/masprog/ice_market_api/models/Product.java
@@ -2,18 +2,14 @@ package com.masprog.ice_market_api.models;
 
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.*;
 
 import java.math.BigDecimal;
 
 @Entity
-@Getter
-@Setter
-@RequiredArgsConstructor
-@NoArgsConstructor
 @Table(name = "tb_products")
-@ToString
 public class Product {
 
     @Id
@@ -21,12 +17,64 @@ public class Product {
     private Long productId;
 
     @NotBlank
-    @Size(min = 2, message = "Product name must contain at least 3 characters")
+    @Size(min = 3, message = "Product name must contain at least 3 characters")
     private String productName;
 
     @NotBlank
     @Size(min = 6, message = "Product description must contain at least 6 characters")
     private String productDescription;
+
     private BigDecimal price;
     private boolean availability;
+
+
+    public Product(){}
+
+    public Product(Long productId, String productName, String productDescription, BigDecimal price, boolean availability) {
+        this.productId = productId;
+        this.productName = productName;
+        this.productDescription = productDescription;
+        this.price = price;
+        this.availability = availability;
+    }
+
+    public Long getProductId() {
+        return productId;
+    }
+
+    public void setProductId(Long productId) {
+        this.productId = productId;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public String getProductDescription() {
+        return productDescription;
+    }
+
+    public void setProductDescription(String productDescription) {
+        this.productDescription = productDescription;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public boolean isAvailability() {
+        return availability;
+    }
+
+    public void setAvailability(boolean availability) {
+        this.availability = availability;
+    }
 }
