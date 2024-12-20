38e7eaa9404e3bb797315dec15acf6a9a1941e63
Mauro Manuel <mauro.manuel@unitel.co.ao>
2024-12-20 17:13:16 +0100

    feat: added addProduct on ProductServiceImpl

--- src/main/java/com/masprog/ice_market_api/services/impl/ProductServiceImpl.java
+++ src/main/java/com/masprog/ice_market_api/services/impl/ProductServiceImpl.java
@@ -1,4 +1,37 @@
 package com.masprog.ice_market_api.services.impl;
 
-public class ProductServiceImpl {
+import com.masprog.ice_market_api.exceptions.APIException;
+import com.masprog.ice_market_api.models.Product;
+import com.masprog.ice_market_api.payload.ProductDTO;
+import com.masprog.ice_market_api.repositories.ProductRepository;
+import com.masprog.ice_market_api.services.ProductService;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProductServiceImpl implements ProductService {
+
+    final
+    ModelMapper modelMapper;
+
+    private final ProductRepository productRepository;
+
+    public ProductServiceImpl(ProductRepository productRepository, ModelMapper modelMapper) {
+
+        this.productRepository = productRepository;
+
+        this.modelMapper = modelMapper;
+    }
+
+    @Override
+    public ProductDTO addProduct(ProductDTO productDTO) {
+        Product product = modelMapper.map(productDTO, Product.class);
+        Product productFromDb = productRepository.findByProductName(product.getProductName());
+        if (productFromDb != null){
+            throw new APIException("Product with the name " + product.getProductName() +" already exists !!!");
+        }
+        Product savedProduct = productRepository.save(product);
+        return modelMapper.map(savedProduct, ProductDTO.class);
+
+    }
 }
