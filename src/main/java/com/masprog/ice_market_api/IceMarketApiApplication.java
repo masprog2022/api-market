60422decec36476c049f8b6c8b7b70225213cea0
Mauro Manuel <mauro.manuel@unitel.co.ao>
2024-12-20 14:07:24 +0100

    first commit

---
+++ src/main/java/com/masprog/ice_market_api/IceMarketApiApplication.java
@@ -0,0 +1,13 @@
+package com.masprog.ice_market_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IceMarketApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IceMarketApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/masprog/ice_market_api/IceMarketApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.masprog.ice_market_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IceMarketApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
