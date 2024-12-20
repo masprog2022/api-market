35f3d6844054ce7cbcbaf7c9c7a25d08076659d5
Mauro Manuel <mauro.manuel@unitel.co.ao>
2024-12-20 18:58:56 +0100

    fix: execptions

--- src/main/java/com/masprog/ice_market_api/exceptions/APIException.java
+++ src/main/java/com/masprog/ice_market_api/exceptions/APIException.java
@@ -4,4 +4,8 @@ public class APIException extends RuntimeException{
     public APIException(String message){
         super(message);
     }
+
+    public APIException(){
+
+    }
 }
--- src/main/java/com/masprog/ice_market_api/payload/APIResponse.java
+++ src/main/java/com/masprog/ice_market_api/payload/APIResponse.java
@@ -4,8 +4,12 @@ import lombok.*;
 
 @Data
 @NoArgsConstructor
-@AllArgsConstructor
 public class APIResponse {
     public String message;
     private boolean status;
+
+    public APIResponse(String message, boolean status){
+        this.message = message;
+        this.status = status;
+    }
 }
