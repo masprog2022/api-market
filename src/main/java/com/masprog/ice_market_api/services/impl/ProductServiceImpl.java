b6fd3f2e5c815fca594bf9e1d97f53c455565afe
Mauro Manuel <mauro.manuel@unitel.co.ao>
2024-12-21 15:08:40 +0100

    feat: added methods getAllProducts on ProductServiceImpl

--- src/main/java/com/masprog/ice_market_api/services/impl/ProductServiceImpl.java
+++ src/main/java/com/masprog/ice_market_api/services/impl/ProductServiceImpl.java
@@ -3,10 +3,17 @@ package com.masprog.ice_market_api.services.impl;
 import com.masprog.ice_market_api.exceptions.APIException;
 import com.masprog.ice_market_api.models.Product;
 import com.masprog.ice_market_api.payload.ProductDTO;
+import com.masprog.ice_market_api.payload.ProductResponse;
 import com.masprog.ice_market_api.repositories.ProductRepository;
 import com.masprog.ice_market_api.services.ProductService;
 import org.modelmapper.ModelMapper;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class ProductServiceImpl implements ProductService {
@@ -35,4 +42,36 @@ public class ProductServiceImpl implements ProductService {
         return modelMapper.map(savedProduct, ProductDTO.class);
 
     }
+
+    @Override
+    public ProductResponse getAllProducts(Integer pageNumber, Integer pageSize, String sortBy, String sortOrder) {
+        Sort sortByAndOrder = sortOrder.equalsIgnoreCase("asc")
+                ? Sort.by(sortBy).ascending()
+                : Sort.by(sortBy).descending();
+
+        Pageable pageDetails = PageRequest.of(pageNumber, pageSize, sortByAndOrder);
+        Page<Product> productPage = productRepository.findAll(pageDetails);
+
+        List<Product> products = productPage.getContent();
+        if (products.isEmpty()){
+            throw new APIException("Nenhum produto encontrado");
+        }
+
+        List<ProductDTO> productDTOS = products.stream()
+                .map(product -> modelMapper.map(product, ProductDTO.class))
+                .toList();
+
+        ProductResponse productResponse = new ProductResponse();
+        productResponse.setContent(productDTOS);
+        productResponse.setPageNumber(productPage.getNumber());
+        productResponse.setPageSize(productPage.getSize());
+        productResponse.setTotalElements(productPage.getTotalElements());
+        productResponse.setTotalPages(productPage.getTotalPages());
+        productResponse.setLastPage(productPage.isLast());
+        return productResponse;
+
+
+    }
+
+
 }
