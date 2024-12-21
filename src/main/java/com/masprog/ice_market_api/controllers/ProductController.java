9eb7fdaffaf5aa21f0e9ac778d6d64817921ebbc
Mauro Manuel <mauro.manuel@unitel.co.ao>
2024-12-21 15:07:00 +0100

    feat: added methods getAllProducts on ProductController

--- src/main/java/com/masprog/ice_market_api/controllers/ProductController.java
+++ src/main/java/com/masprog/ice_market_api/controllers/ProductController.java
@@ -1,14 +1,13 @@
 package com.masprog.ice_market_api.controllers;
 
+import com.masprog.ice_market_api.config.AppConstants;
 import com.masprog.ice_market_api.payload.ProductDTO;
+import com.masprog.ice_market_api.payload.ProductResponse;
 import com.masprog.ice_market_api.services.ProductService;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api")
@@ -25,5 +24,16 @@ public class ProductController {
         ProductDTO savedProductDTO = productService.addProduct(productDTO);
         return new ResponseEntity<>(savedProductDTO, HttpStatus.CREATED);
     }
+
+    @GetMapping("/products")
+    public ResponseEntity<ProductResponse> getAllProducts(
+            @RequestParam(name = "pageNumber", defaultValue = AppConstants.PAGE_NUMBER, required = false) Integer pageNumber,
+            @RequestParam(name = "pageSize", defaultValue = AppConstants.PAGE_SIZE, required = false) Integer pageSize,
+            @RequestParam(name = "sortBy", defaultValue = AppConstants.SORT_PRODUCTS_BY, required = false) String sortBy,
+            @RequestParam(name = "sortOrder", defaultValue = AppConstants.SORT_DIR, required = false) String sortOrder) {
+       ProductResponse productResponse = productService.getAllProducts(pageNumber, pageSize, sortBy, sortOrder);
+        return new ResponseEntity<>(productResponse, HttpStatus.OK);
+    }
+
 }
 
