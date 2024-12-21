d9b87e851bdfa1144fdba51fa00c3153fad077c9
Mauro Manuel <mauro.manuel@unitel.co.ao>
2024-12-21 15:09:59 +0100

    feat: added methods getAllProducts on ProductService

--- src/main/java/com/masprog/ice_market_api/services/ProductService.java
+++ src/main/java/com/masprog/ice_market_api/services/ProductService.java
@@ -1,9 +1,11 @@
 package com.masprog.ice_market_api.services;
 
 import com.masprog.ice_market_api.payload.ProductDTO;
+import com.masprog.ice_market_api.payload.ProductResponse;
 
 public interface ProductService {
 
     ProductDTO addProduct(ProductDTO product);
+    ProductResponse getAllProducts(Integer pageNumber, Integer pageSize, String sortBy, String sortOrder);
 
 }
