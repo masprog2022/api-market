b1d20756eca0c22a579055934ac1490ef9944e30
Mauro Manuel <mauro.manuel@unitel.co.ao>
2024-12-23 14:45:27 +0100

    feat: Added UserRepository

--- src/main/java/com/masprog/ice_market_api/repositories/UserRepository.java
+++ src/main/java/com/masprog/ice_market_api/repositories/UserRepository.java
@@ -1,4 +1,12 @@
 package com.masprog.ice_market_api.repositories;
 
-public interface UserRepository {
+import com.masprog.ice_market_api.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUserName(String username);
+    Boolean existsByUserName(String username);
+    Boolean existsByEmail(String email);
 }
