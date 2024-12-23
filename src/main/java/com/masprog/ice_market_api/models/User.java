3411ce7df78a73a7245ab448c1c36fe74863173a
Mauro Manuel <mauro.manuel@unitel.co.ao>
2024-12-23 12:29:23 +0100

    feat: added entity User

---
+++ src/main/java/com/masprog/ice_market_api/models/User.java
@@ -0,0 +1,110 @@
+package com.masprog.ice_market_api.models;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.*;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+@Entity
+@Table(name = "tb_users",
+        uniqueConstraints = {
+                @UniqueConstraint(columnNames = "username"),
+                @UniqueConstraint(columnNames = "email")
+        })
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private Long userId;
+
+    @NotBlank
+    @Size(max = 20)
+    @Column(name = "username")
+    private String userName;
+
+    @NotBlank
+    @Size(max = 50)
+    @Email
+    @Column(name = "email")
+    private String email;
+
+    @NotBlank
+    @Size(max = 120)
+    @Column(name = "password")
+    private String password;
+
+    @ManyToMany(cascade = {CascadeType.PERSIST}, fetch = FetchType.EAGER)
+    @JoinTable(name = "user_role", joinColumns = @JoinColumn(name = "user_id"),
+    inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private Set<Role> roles = new HashSet<>();
+
+    @OneToMany(mappedBy = "user", cascade = {CascadeType.PERSIST, CascadeType.MERGE}, orphanRemoval = true)
+    private List<Address> addressList = new ArrayList<>();
+
+    public User(){}
+
+    public User(Long userId, String userName, String email, String password) {
+        this.userId = userId;
+        this.userName = userName;
+        this.email = email;
+        this.password = password;
+    }
+
+    public User(String username, String email, String password) {
+        this.userName = username;
+        this.email = email;
+        this.password = password;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public @NotBlank @Size(max = 20) String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(@NotBlank @Size(max = 20) String userName) {
+        this.userName = userName;
+    }
+
+    public @NotBlank @Size(max = 50) @Email String getEmail() {
+        return email;
+    }
+
+    public void setEmail(@NotBlank @Size(max = 50) @Email String email) {
+        this.email = email;
+    }
+
+    public @NotBlank @Size(max = 120) String getPassword() {
+        return password;
+    }
+
+    public void setPassword(@NotBlank @Size(max = 120) String password) {
+        this.password = password;
+    }
+
+    public Set<Role> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(Set<Role> roles) {
+        this.roles = roles;
+    }
+
+    public List<Address> getAddressList() {
+        return addressList;
+    }
+
+    public void setAddressList(List<Address> addressList) {
+        this.addressList = addressList;
+    }
+}
