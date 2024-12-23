c72016a3665f6c201379f8b66444e18cd9a7ac9b
Mauro Manuel <mauro.manuel@unitel.co.ao>
2024-12-23 10:59:58 +0100

    feat: added Role

--- src/main/java/com/masprog/ice_market_api/models/Role.java
+++ src/main/java/com/masprog/ice_market_api/models/Role.java
@@ -1,4 +1,21 @@
 package com.masprog.ice_market_api.models;
 
+import jakarta.persistence.*;
+import lombok.ToString;
+
 public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "role_id")
+    private Long
+            roleId;
+    @ToString.Exclude
+    @Enumerated(EnumType.STRING)
+    @Column(length = 20, name = "role_name")
+    private AppRole roleName;
+
+    public Role(AppRole roleName){
+        this.roleName = roleName;
+    }
 }
