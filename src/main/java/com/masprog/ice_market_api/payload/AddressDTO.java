47c58ef5d521cd2871caebc85391e8ac4499d1a3
Mauro Manuel <mauro.manuel@unitel.co.ao>
2024-12-23 14:41:02 +0100

    feat: added AddressDTO

--- src/main/java/com/masprog/ice_market_api/payload/AddressDTO.java
+++ src/main/java/com/masprog/ice_market_api/payload/AddressDTO.java
@@ -1,4 +1,71 @@
 package com.masprog.ice_market_api.payload;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
 public class AddressDTO {
+    private Long addressId;
+    private String street;
+    private String municipality;
+    private String province;
+    private String country;
+    private String postalCode;
+
+    public AddressDTO() {}
+
+    public AddressDTO(String street, String municipality, String province, String country, String postalCode) {
+        this.street = street;
+        this.municipality = municipality;
+        this.province = province;
+        this.country = country;
+        this.postalCode = postalCode;
+    }
+
+    public Long getAddressId() {
+        return addressId;
+    }
+
+    public void setAddressId(Long addressId) {
+        this.addressId = addressId;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getMunicipality() {
+        return municipality;
+    }
+
+    public void setMunicipality(String municipality) {
+        this.municipality = municipality;
+    }
+
+    public String getProvince() {
+        return province;
+    }
+
+    public void setProvince(String province) {
+        this.province = province;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getPostalCode() {
+        return postalCode;
+    }
+
+    public void setPostalCode(String postalCode) {
+        this.postalCode = postalCode;
+    }
 }
