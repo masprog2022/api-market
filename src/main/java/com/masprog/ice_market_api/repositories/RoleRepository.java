d91d261404a9b22047e1c75109b77ca0a967f684
Mauro Manuel <mauro.manuel@unitel.co.ao>
2024-12-23 14:43:08 +0100

    feat: added RoleRepository

--- src/main/java/com/masprog/ice_market_api/repositories/RoleRepository.java
+++ src/main/java/com/masprog/ice_market_api/repositories/RoleRepository.java
@@ -1,4 +1,11 @@
 package com.masprog.ice_market_api.repositories;
 
-public interface RoleRepository {
+import com.masprog.ice_market_api.models.AppRole;
+import com.masprog.ice_market_api.models.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface RoleRepository extends JpaRepository<Role, Long> {
+    Optional<Role> findByRoleName(AppRole appRole);
 }
