a04bc5f2dec60368403f56ead1995f70072650dc
Mauro Manuel <mauro.manuel@unitel.co.ao>
2024-12-24 00:38:44 +0100

    feat: added authenticateUser, registerUser, currentUsername, getUserDetails on authController

--- src/main/java/com/masprog/ice_market_api/controllers/AuthController.java
+++ src/main/java/com/masprog/ice_market_api/controllers/AuthController.java
@@ -1,4 +1,190 @@
 package com.masprog.ice_market_api.controllers;
 
+import com.masprog.ice_market_api.models.AppRole;
+import com.masprog.ice_market_api.models.Role;
+import com.masprog.ice_market_api.models.User;
+import com.masprog.ice_market_api.payload.ProductDTO;
+import com.masprog.ice_market_api.repositories.RoleRepository;
+import com.masprog.ice_market_api.repositories.UserRepository;
+import com.masprog.ice_market_api.security.jwt.JwtUtils;
+import com.masprog.ice_market_api.security.request.LoginRequest;
+import com.masprog.ice_market_api.security.request.SignupRequest;
+import com.masprog.ice_market_api.security.response.MessageResponse;
+import com.masprog.ice_market_api.security.response.UserInfoResponse;
+import com.masprog.ice_market_api.security.services.UserDetailsImpl;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseCookie;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.security.core.Authentication;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+@Tag(name = "Usuário e autenticação", description = "Endpoints para gerir usuário e autenticação" )
+@RestController
+@RequestMapping("/api/auth")
 public class AuthController {
+
+    @Autowired
+    private JwtUtils jwtUtils;
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Autowired
+    RoleRepository roleRepository;
+
+    @Autowired
+    PasswordEncoder encoder;
+
+    @Operation(summary = "Fazer Login", description = "Qualquer pessoa pode fazer login",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Usuário criado com sucesso ",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = UserInfoResponse.class)))
+            })
+    @PostMapping("/signin")
+    public ResponseEntity<?> authenticateUser(@RequestBody LoginRequest loginRequest) {
+        Authentication authentication;
+        try {
+            authentication = authenticationManager
+                    .authenticate(new UsernamePasswordAuthenticationToken(loginRequest.getUsername(), loginRequest.getPassword()));
+        } catch (AuthenticationException exception) {
+            Map<String, Object> map = new HashMap<>();
+            map.put("message", "Bad credentials");
+            map.put("status", false);
+            return new ResponseEntity<Object>(map, HttpStatus.NOT_FOUND);
+        }
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+
+        UserDetailsImpl userDetails = (UserDetailsImpl) authentication.getPrincipal();
+
+        ResponseCookie jwtCookie = jwtUtils.generateJwtCookie(userDetails);
+
+        List<String> roles = userDetails.getAuthorities().stream()
+                .map(GrantedAuthority::getAuthority)
+                .collect(Collectors.toList());
+
+        UserInfoResponse response = new UserInfoResponse(userDetails.getId(),
+                userDetails.getUsername(), roles, jwtCookie.toString());
+
+        return ResponseEntity.ok().header(HttpHeaders.SET_COOKIE,
+                        jwtCookie.toString())
+                .body(response);
+    }
+
+    @Operation(summary = "Criar um usuário", description = "Qualquer pessoa pode se cadastrar",
+            responses = {
+                    @ApiResponse(responseCode = "201", description = "Usuário criado com sucesso ",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = MessageResponse.class)))
+            })
+    @PostMapping("/signup")
+    public ResponseEntity<?> registerUser(@Valid @RequestBody SignupRequest signUpRequest) {
+        if (userRepository.existsByUserName(signUpRequest.getUsername())) {
+            return ResponseEntity.badRequest().body(new MessageResponse("Error: Username is already taken!"));
+        }
+
+        if (userRepository.existsByEmail(signUpRequest.getEmail())) {
+            return ResponseEntity.badRequest().body(new MessageResponse("Error: Email is already in use!"));
+        }
+
+        // Create new user's account
+        User user = new User(signUpRequest.getUsername(),
+                signUpRequest.getEmail(),
+                encoder.encode(signUpRequest.getPassword()));
+
+        Set<String> strRoles = signUpRequest.getRole();
+        Set<Role> roles = new HashSet<>();
+
+        if (strRoles == null) {
+            Role userRole = roleRepository.findByRoleName(AppRole.ROLE_USER)
+                    .orElseThrow(() -> new RuntimeException("Error: Role is not found."));
+            roles.add(userRole);
+        } else {
+            strRoles.forEach(role -> {
+                switch (role) {
+                    case "admin":
+                        Role adminRole = roleRepository.findByRoleName(AppRole.ROLE_ADMIN)
+                                .orElseThrow(() -> new RuntimeException("Error: Role is not found."));
+                        roles.add(adminRole);
+
+                        break;
+
+                    default:
+                        Role userRole = roleRepository.findByRoleName(AppRole.ROLE_USER)
+                                .orElseThrow(() -> new RuntimeException("Error: Role is not found."));
+                        roles.add(userRole);
+                }
+            });
+        }
+
+        user.setRoles(roles);
+        userRepository.save(user);
+
+        return ResponseEntity.ok(new MessageResponse("User registered successfully!"));
+    }
+
+    @Operation(summary = "Nome do usuário logado", description = "Qualquer pessoa pode ver o usuário logado",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Usuário criado com sucesso ",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = String.class)))
+            })
+    @GetMapping("/username")
+    public String currentUserName(Authentication authentication){
+        if (authentication != null)
+            return authentication.getName();
+        else
+            return "";
+    }
+
+    @Operation(summary = "Detalhe do usuário logado", description = "Qualquer pessoa pode se pode ver detalhe dos seus dados",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Detalhes do usuário com sucesso ",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = UserInfoResponse.class)))
+            })
+    @GetMapping("/user")
+    public ResponseEntity<?> getUserDetails(Authentication authentication){
+        UserDetailsImpl userDetails = (UserDetailsImpl) authentication.getPrincipal();
+
+        List<String> roles = userDetails.getAuthorities().stream()
+                .map(GrantedAuthority::getAuthority)
+                .collect(Collectors.toList());
+
+        UserInfoResponse response = new UserInfoResponse(userDetails.getId(),
+                userDetails.getUsername(), roles);
+
+        return ResponseEntity.ok().body(response);
+    }
+
+    @Operation(summary = "Fazer logout", description = "Qualquer pessoa pode se cadastrar",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Usuário criado com sucesso ",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = MessageResponse.class)))
+            })
+    @PostMapping("/signout")
+    public ResponseEntity<?> signoutUser(){
+        ResponseCookie cookie = jwtUtils.getCleanJwtCookie();
+        return ResponseEntity.ok().header(HttpHeaders.SET_COOKIE,
+                        cookie.toString())
+                .body(new MessageResponse("You've been signed out!"));
+    }
+
 }
