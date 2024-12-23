3245c4002dc75b72e90c3efc03db51c25b5f89f3
Mauro Manuel <mauro.manuel@unitel.co.ao>
2024-12-23 09:55:27 +0100

    added SpringDocOpenApiConfig

---
+++ src/main/java/com/masprog/ice_market_api/docment/SpringDocOpenApiConfig.java
@@ -0,0 +1,34 @@
+package com.masprog.ice_market_api.docment;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SpringDocOpenApiConfig {
+
+
+    @Bean
+    public OpenAPI openAPI(){
+        return new OpenAPI()
+                .components(new Components())
+                .info(new Info()
+                        .title("Rest API - Ice Market api ")
+                        .version("v1")
+                        .description("Api para venda especifica de gelo")
+                        .termsOfService("https://about-mauro.netlify.app/")
+                        .license(
+                                new License()
+                                        .name("GNU GENERAL PUBLIC LICENSE")
+                                        .url("https://www.gnu.org/licenses/gpl-3.0.pt-br.html")
+                        )
+                        .contact(new Contact().name("Mauro Manuel").url("https://www.linkedin.com/in/mauro-manuel-522947b2/"))
+                );
+    }
+
+
+}
