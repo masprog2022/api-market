4991c54397e9ed726b75d43d44110ba001b2945e
Mauro Manuel <mauro.manuel@unitel.co.ao>
2024-12-23 09:56:30 +0100

    feat: addded config swagger on ProductController

--- src/main/java/com/masprog/ice_market_api/controllers/ProductController.java
+++ src/main/java/com/masprog/ice_market_api/controllers/ProductController.java
@@ -4,11 +4,17 @@ import com.masprog.ice_market_api.config.AppConstants;
 import com.masprog.ice_market_api.payload.ProductDTO;
 import com.masprog.ice_market_api.payload.ProductResponse;
 import com.masprog.ice_market_api.services.ProductService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+@Tag(name = "Produtos", description = "Endpoints para criar, actualizar, listar e deletar produtos" )
 @RestController
 @RequestMapping("/api")
 public class ProductController {
@@ -19,12 +25,22 @@ public class ProductController {
         this.productService = productService;
     }
 
+    @Operation(summary = "Criar Produto", description = "Requisição feita somente pelos ADMIN do sistema",
+            responses = {
+                    @ApiResponse(responseCode = "201", description = "Produto criado com sucesso ",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = ProductDTO.class)))
+            })
     @PostMapping("/products")
     public ResponseEntity<ProductDTO> createProduct(@Valid @RequestBody ProductDTO productDTO){
         ProductDTO savedProductDTO = productService.addProduct(productDTO);
         return new ResponseEntity<>(savedProductDTO, HttpStatus.CREATED);
     }
 
+    @Operation(summary = "Listar todos Produtos", description = "Todos podem ver os produtos",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Produto listado com sucesso ",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = ProductResponse.class)))
+            })
     @GetMapping("/products")
     public ResponseEntity<ProductResponse> getAllProducts(
             @RequestParam(name = "pageNumber", defaultValue = AppConstants.PAGE_NUMBER, required = false) Integer pageNumber,
@@ -35,12 +51,23 @@ public class ProductController {
         return new ResponseEntity<>(productResponse, HttpStatus.OK);
     }
 
+    @Operation(summary = "Actualizar Produto", description = "Requisição feita somente pelos ADMIN do sistema",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Produto actualizado com sucesso ",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = ProductDTO.class)))
+            })
     @PutMapping("/products/{productId}")
     public ResponseEntity<ProductDTO> updateProduct(@Valid @RequestBody ProductDTO productDTO, @PathVariable Long productId){
         ProductDTO savedProductDTO = productService.updateProduct(productDTO, productId);
         return new ResponseEntity<>(savedProductDTO, HttpStatus.OK);
     }
 
+    @Operation(summary = "Remover Produto", description = "Requisição feita somente pelos ADMIN do sistema",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Produto removido com sucesso ",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = ProductDTO.class)))
+            })
+    @DeleteMapping("/products/{productId}")
     public ResponseEntity<ProductDTO> deleteProduct(@PathVariable Long productId){
         ProductDTO deleteProduct = productService.deleteProduct(productId);
         return new ResponseEntity<>(deleteProduct, HttpStatus.OK);
