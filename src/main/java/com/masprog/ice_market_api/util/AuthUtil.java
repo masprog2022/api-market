4584ad4409496215912e820072172dcbd3ef9171
Mauro Manuel <mauro.manuel@unitel.co.ao>
2024-12-26 09:07:11 +0100

    feat: added AuthUtil

---
+++ src/main/java/com/masprog/ice_market_api/util/AuthUtil.java
@@ -0,0 +1,42 @@
+package com.masprog.ice_market_api.util;
+
+import com.masprog.ice_market_api.models.User;
+import com.masprog.ice_market_api.repositories.UserRepository;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AuthUtil {
+
+    private final UserRepository userRepository;
+
+    public AuthUtil(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public String loggedInEmail(){
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        User user = userRepository.findByUserName(authentication.getName())
+                .orElseThrow(()-> new UsernameNotFoundException("User not found with username: " +authentication.getName()));
+
+        return user.getEmail();
+    }
+
+    public Long loggedInUserId(){
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        User user = userRepository.findByUserName(authentication.getName())
+                .orElseThrow(() -> new UsernameNotFoundException("User Not Found with username: " + authentication.getName()));
+
+        return user.getUserId();
+    }
+
+    public User loggedInUser(){
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        return userRepository.findByUserName(authentication.getName())
+                .orElseThrow(() -> new UsernameNotFoundException("User Not Found with username: " + authentication.getName()));
+    }
+}
