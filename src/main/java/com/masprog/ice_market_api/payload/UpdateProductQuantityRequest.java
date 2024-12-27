f89e047e3bc73489ccc50888aa2587ab389c0829
Mauro Manuel <mauro.manuel@unitel.co.ao>
2024-12-27 15:22:38 +0100

    feat: added UpdateProductQuantityRequest

--- src/main/java/com/masprog/ice_market_api/payload/UpdateProductQuantityRequest.java
+++ src/main/java/com/masprog/ice_market_api/payload/UpdateProductQuantityRequest.java
@@ -1,4 +1,15 @@
 package com.masprog.ice_market_api.payload;
 
 public class UpdateProductQuantityRequest {
+
+    private Integer quantity;
+
+    // Getters e setters
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
 }
