4f25583c57d4825840366f3d5e9ba781db3fa965
Mauro Manuel <mauro.manuel@unitel.co.ao>
2024-12-27 10:50:00 +0100

    feat: added getAllCarts on CartServiceImpl

--- src/main/java/com/masprog/ice_market_api/services/impl/CartServiceImpl.java
+++ src/main/java/com/masprog/ice_market_api/services/impl/CartServiceImpl.java
@@ -18,6 +18,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 @Service
@@ -93,7 +94,28 @@ public class CartServiceImpl implements CartService {
 
     @Override
     public List<CartDTO> getAllCarts() {
-        return List.of();
+        List<Cart> carts = cartRepository.findAll();
+
+        if (carts.isEmpty()){
+            throw new APIException("No cart exists");
+        }
+
+        return carts.stream().map(cart -> {
+            CartDTO cartDTO = modelMapper.map(cart, CartDTO.class);
+
+            List<ProductDTO> products = cart.getCartItems().stream().map(cartItem -> {
+                ProductDTO productDTO = modelMapper.map(cartItem.getProduct(), ProductDTO.class);
+                productDTO.setQuantity(cartItem.getQuantity()); // Set the quantity from CartItem
+                return productDTO;
+            }).collect(Collectors.toList());
+
+
+            cartDTO.setProducts(products);
+
+            return cartDTO;
+
+        }).collect(Collectors.toList());
+
     }
 
     @Override
