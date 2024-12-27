bbba42228324d95147dde7e02ed9674ed8092d1f
Mauro Manuel <mauro.manuel@unitel.co.ao>
2024-12-27 09:05:51 +0100

    feat: added CartService

--- src/main/java/com/masprog/ice_market_api/services/CartService.java
+++ src/main/java/com/masprog/ice_market_api/services/CartService.java
@@ -1,4 +1,23 @@
 package com.masprog.ice_market_api.services;
 
-public class CartService {
+
+import com.masprog.ice_market_api.payload.CartDTO;
+import org.springframework.transaction.annotation.Transactional;
+
+
+import java.util.List;
+
+public interface CartService {
+
+    CartDTO addProductToCart(Long productId, Integer quantity);
+    List<CartDTO> getAllCarts();
+    CartDTO getCart(String emailId, Long cartId);
+
+    @Transactional
+    CartDTO updateProductQuantityInCart(Long productId, Integer quantity);
+
+    @Transactional
+    String deleteProductFromCart(Long cartId, Long productId);
+
+    void updateProductInCarts(Long cartId, Long productId);
 }
